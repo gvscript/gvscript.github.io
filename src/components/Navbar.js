86891560bd98a36d0e4a5e76d0099804cebe6b2e
Gabriel Viana <g.viana87@hotmail.com>
2021-11-02 23:54:05 -0300

    navbar ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
 
-        <a className="navbar-brand" href="#"><img className="logo" alt="GV Logo" width="200px" src={gvLogo} /></a>
+      <a className="navbar-brand" href="#"><img className="logo" alt="GV Logo" src={gvLogo} /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,7 +17,19 @@ const Navbar = () => {
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#">Sobre</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Habilidades</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Experiência</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfólio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contato</a>
             </li>
           </ul>
         </div>
