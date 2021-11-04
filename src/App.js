7c222254199a367357d51758a617b2c0689fd976
Gabriel Viana <g.viana87@hotmail.com>
2021-11-03 22:18:58 -0300

    nav fixed?

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Sobre from './components/Sobre';
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Sobre />
     </>
   );
 }
