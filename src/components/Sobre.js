bf4b3635397b9c59b3bd68c93b1e0e4969ee5c77
Gabriel Viana <g.viana87@hotmail.com>
2021-11-04 00:48:56 -0300

    corrige tamanho bg header/ começaadd txt sobre

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -10,15 +10,32 @@ const Sobre = () => {
 
         <img alt="Foto Gabriel" src={foto}/>
         <div className="about-me">
-        <p>Veja só as propriedades unidas e como o texto para definição e/ou alteração da borda de um site fica:
-
-        border: border-width border-style border-color (ou seja: tamanho da borda estilo da borda cor utilizada na borda)
-        No entanto, na hora de montar o texto, é  preciso que a sintaxe de cada uma das propriedades seja feita de forma resumida. Por exemplo:
-
-        border: border-width border-style border-color” ficaria “border: 5px solid black”;
-        Para aplicar a largura da borda, é usado o atributo border-width. Veja como ficaria:</p>
-        <h3>Qualidades:</h3>
-        <Type />
+        <h3>I have a dream</h3>
+        <h3>I´m a dreamer</h3>
+        <p>
+          Eu sou o Gabriel, paulistano residente da Brasilândia zona norte de SP.<br/>
+          Um apaixonado por histórias de pessoas reais com o objetivo de utilizar a tecnologia como ferramenta de transformação, de inclusão social para um mundo melhor e mais igualitário.
+          <br/>
+          Sou formado em Publicidade e Propaganda pela PUC/SP e estudante de Desenvolvimento Web Full-Stack na Trybe.
+        </p>
+        <p>
+          Uma palavra: Gratidão - por cada dia cheio de possibilidades de me tornar um ser humano melhor que ontem<br/>
+          Uma frase: "Amar e mudar as coisas me interesa mais" - Belchior
+        </p>
+        <h4>Qualidades:</h4>
+        <Type 
+          className="about-typeText"
+          strings={[
+            'Comunicação',
+            'Respeito',
+            'Empatia',
+            'Humildade',
+            'Senso coletivo',
+          ]}
+          typeSpeed={40}
+          backSpeed={50}
+          loop
+        />
         </div>
       </div>
     </div>
