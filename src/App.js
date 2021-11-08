8e37d5fcc81dfb74db3922497ca1903eea1c9e96
Gabriel Viana <g.viana87@hotmail.com>
2021-11-08 10:46:57 -0300

    refatora rota das seçoes atraves de arq index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './Seções/Navbar';
-import Header from './Seções/Header';
-import Sobre from './Seções/Sobre';
-import Habilidades from './Seções/Habilidades';
+import {
+  Navbar,
+  Header,
+  Sobre,
+  Habilidades
+} from './Seções'
 
 function App() {
   return (
